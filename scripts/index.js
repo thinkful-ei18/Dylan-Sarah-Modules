5474886cc26416949ed6a4a539e7c2b2dc79e1c5
Dylan <nielsen.dylan@gmail.com>
2018-01-22 09:03:26 -0800

    Add Item.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
 /* global shoppingList, cuid */
 
 // eslint-disable-next-line no-unused-vars
-
+'use strict';
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 });
-
